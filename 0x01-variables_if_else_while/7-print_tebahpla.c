d1bab7dc056af7a47fac3f1e76112765a93ec229
swar1999 <muzan22kris@gmail.com>
2023-08-11 16:35:21 -0700

    task 7 while statment

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: programe that print alphabet in lowercast
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
