db8c02289a15399094d8d7d03e10ecc51b30642a
swar1999 <muzan22kris@gmail.com>
2023-08-11 15:57:30 -0700

    task 2 while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *  Description: program to print alphabet in lower cast
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
