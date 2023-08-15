8384b5f326d15a6ba0c769baab740368cb9e6281
swar1999 <muzan22kris@gmail.com>
2023-08-15 16:38:31 -0700

    task7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Entry point
+ * Description: program that calculate the absalut value
+ * @int : integer
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+	int i;
+		_abs(i);
+}
