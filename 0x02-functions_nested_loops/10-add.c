bd93dc8850e5495ccc2115326249ee572fd19852
swar1999 <muzan22kris@gmail.com>
2023-08-15 17:46:46 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: program that add two numbers
+ * @n: paramter1
+ * @m: paramter2
+ * Return: Always 0 (Success)
+ */
+int add(int n, int m)
+{
+	int sum = n + m;
+		return (sum);
+}
