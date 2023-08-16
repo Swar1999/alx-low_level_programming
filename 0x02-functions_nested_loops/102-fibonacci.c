4a4e7ef9988debb1eade4b77d5f98cc98be5b6e1
swar1999 <muzan22kris@gmail.com>
2023-08-16 15:40:56 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program that prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 1;
+	int next;
+	int i;
+		printf("%d, %d,", n1, n2);
+		for (i = 3; i <= 50; i++)
+		{
+			next = n1 + n2;
+			printf("%d,", next);
+			n1 = n2;
+			n2 = next;
+		}
+		return (0);
+}
