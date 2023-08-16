233b134c065555681ef80392ee2a8ccc6be024b4
swar1999 <muzan22kris@gmail.com>
2023-08-16 16:03:15 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: program that prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long f1 = 0, f2 = 1, sum;
+	float total_sum;
+
+	while (1)
+	{
+		sum = f1 + f2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		f1 = f2;
+		f2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
