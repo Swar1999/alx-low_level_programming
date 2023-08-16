fa586de03392386ba97db97ca6bf429654b71751
swar1999 <muzan22kris@gmail.com>
2023-08-16 15:07:38 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: program that calculate the multiply of
+ * 3 & 5 blow 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, sum
+		for (n = 1; n < 1024; n++)
+		{
+			if ((n % 3 == 0 || n % 5 == 0))
+				sum += n;
+		}
+	printf("%d\n", sum);
+	return (0);
+}
+
+
