456d9188ca587cd1f6122b987470c03ec60012fe
swar1999 <muzan22kris@gmail.com>
2023-08-17 14:44:15 -0700

    task 5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,9 @@ void print_most_numbers(void)
 		for (n = 0; n < 10; n++)
 		{
 			if (n != 2 || n != 4)
-			{
-				_putchar(n + 48);
-			}
-			_putchar('\n');
-		}
+				continue;
+			_putchar(n + 48);
+		}	
+		_putchar('\n');
 }
 
