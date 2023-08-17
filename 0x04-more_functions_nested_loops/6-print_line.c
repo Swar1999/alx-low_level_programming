74ead581802bd5761536de0dba705da4792ce4d2
swar1999 <muzan22kris@gmail.com>
2023-08-17 15:29:52 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description: program that prints line
+ * @n : number of -
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	for (n = 1; n <= 10; n++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar(95);
+	}
+	_putchar('\n');
+}
+
+
+
+
