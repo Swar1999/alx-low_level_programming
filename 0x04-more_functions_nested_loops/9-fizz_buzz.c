bd9154afe8dccaa966c4f87afcd36e559e7bbc44
swar1999 <muzan22kris@gmail.com>
2023-08-17 17:18:16 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,16 +8,14 @@
 int main(void)
 {
 	int i;
-	char str1[] = "Fizz";
-	char str2[] = "Buzz";
 		for (i = 1; i <= 100; i++)
 		{
 			if (i % 3 == 0 && !(i % 5 == 0))
-				printf("%s\n", str1);
+				printf("Fizz");
 			else if (i % 5 == 0 && !(i % 3 == 0))
-				printf("%s\n", str2);
+				printf("Buzz");
 			else if (i % 3 == 0 && i % 5 == 0)
-				printf("%s\n", str1, str2);
+				printf("FizzBuzz");
 			else
 				printf("%d", i);
 			if (i != 100)
