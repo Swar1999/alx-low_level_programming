9f800b530de803159811594acdc919430df37302
swar1999 <muzan22kris@gmail.com>
2023-08-22 15:43:50 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * Description: program that swap two ints
+ * @a: pointer 1
+ * @b :pointer 2
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int t;
+		t = *a;
+		*a = *b;
+		*b = t;
+}
