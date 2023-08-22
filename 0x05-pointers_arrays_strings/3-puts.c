a198862730045566762e2f7ddaa35909babe0b94
swar1999 <muzan22kris@gmail.com>
2023-08-22 16:09:31 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * Description: program that prints length of string
+ * @str :pointer
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
+
