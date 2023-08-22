1920a0322654751b0d68187b1037aec20d1c2c3e
swar1999 <muzan22kris@gmail.com>
2023-08-22 15:32:05 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Description: program that points and update value to 98
+ * @n: pointer
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
