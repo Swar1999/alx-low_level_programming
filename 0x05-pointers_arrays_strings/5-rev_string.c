5eed568dcb4e505ee4725f6d45423bba033680c9
swar1999 <muzan22kris@gmail.com>
2023-08-22 16:24:05 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * Description: program that prints string in revres
+ * @s: pointer
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int n = 0;
+	int i;
+		while (s[n] != '\0')
+			n++;
+		for (i = 0; i < n; i++)
+		{
+			n--;
+			rev = s[i];
+			s[i] = s[n];
+			s[n] = rev;
+		}
+}
