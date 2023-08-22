00dce187826d4fc407f5fa26eccfc0ae238779f0
swar1999 <muzan22kris@gmail.com>
2023-08-22 16:36:06 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * Description: program that prints string follow by new line
+ * @str: pointer
+ * Return: Always 0 (Success)
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	char *s = str;
+	int n;
+		while (*s != '\0')
+		{
+			s++;
+			len++;
+		}
+		t = len - 1;
+		for (n = 0; n <= t; n++)
+		{
+			if (n % 2 == 0)
+				_putchar(str[n]);
+		}
+		_putchar('\0');
+}
