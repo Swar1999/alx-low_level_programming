728da9da0ec3eac377bf0ae6d7722f23cfb3ab68
swar1999 <muzan22kris@gmail.com>
2023-08-28 12:26:32 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * Description: program that copies memory area
+ * @dest: pointer to area 2
+ * @src: pointer to area 1
+ * @n: number of bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+		for (; i < n; i++)
+		{
+			dest[i] = src[i];
+			n--;
+		}
+		return (dest);
+}
