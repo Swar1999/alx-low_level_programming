394b9de304d3ce9ec04225de45d5f3c87be75bbd
swar1999 <muzan22kris@gmail.com>
2023-09-04 15:52:22 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argc : counter
  * Return: Always 0 (Success)
  */
-void main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
