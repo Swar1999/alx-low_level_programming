646362548d958d98666b4c98d98691920fc20faf
swar1999 <muzan22kris@gmail.com>
2023-09-07 15:57:58 -0700

    task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - Entry point
+ * Description: program that reallocates a memory block using malloc and free
+ * @ptr : pointer to new address
+ * @old_size : previous size of memory
+ * @new_size : new size of memory
+ * Return: Always 0 (Success)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+		if (new_size == old_size)
+			return (ptr);
+		if (new_size == 0 && ptr)
+			free(ptr);
+		return (NULL);
+		if (!ptr)
+			return (malloc(new_size));
+		ptr1 = malloc(new_size);
+		if (!ptr1)
+			return (NULL);
+		old_ptr = ptr;
+		if (new_size < old_size)
+		{
+			for (i = 0; i < new_size; i++)
+				ptr1[i] = old_ptr[i];
+		}
+		if (new_size > old_size)
+		{
+			for (i = 0; i < old_size; i++)
+				ptr1[i] = old_ptr[i];
+		}
+		free(ptr);
+		return (ptr1);
+}
