f7a19b141a54d28a1ef25cc63759ce2117410b2a
swar1999 <muzan22kris@gmail.com>
2023-09-12 15:18:00 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* init_dog - main entry
+*description : function that initialize structur
+* @d :struct type
+* @name :char
+* @age :float
+* @owner :char
+* return : always 0 (success)
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
