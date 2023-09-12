e439207aba5be7ee419c737581be51a1f737f382
swar1999 <muzan22kris@gmail.com>
2023-09-12 14:46:33 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - contain 3 members
+ * @name : frist member
+ * @age : second member
+ * @owner : thired member
+ * Description: just definition
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
