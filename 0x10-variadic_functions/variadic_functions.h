24a3373cafc963dc6b7f950513e97b72e242e533
swar1999 <muzan22kris@gmail.com>
2023-09-14 13:57:43 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTION_H
+#ifndef VARIADIC_H_
+#define VARIADIC_H_
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
