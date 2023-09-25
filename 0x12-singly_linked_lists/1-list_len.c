014608ac7570b288b555d2b261214913088c66c9
swar1999 <muzan22kris@gmail.com>
2023-09-25 05:32:37 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_len - function that print number of elements in list
+ * @h : struct paramter
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+		while (h->next != NULL)
+		{
+			h = h->next;
+			counter++;
+		}
+		return (counter);
+}
