8f04cac53700442e55eb503c0e006096b82a3058
swar1999 <muzan22kris@gmail.com>
2023-09-25 16:28:35 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - free list
+ * @head : pointer
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *swp;
+		if (head == NULL) /*already setup */
+			return; /*return nothing*/
+		while (head)
+		{
+			swp = (*head)->next;/*let it points to next node*/
+			free(*head);/*free space*/
+			*head = swp;
+		}
+		*head = NULL;
+}
