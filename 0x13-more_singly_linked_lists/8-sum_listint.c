6e16fbb04157a9c08cf82646af324c11ee5980f3
swar1999 <muzan22kris@gmail.com>
2023-09-26 12:34:31 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * sum_listint - function that prints all data sum
+ * @head : struct paramter
+ * Return: sum of data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+		if (head == NULL)
+			return (0);
+		while (head)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+		return (sum);
+}
