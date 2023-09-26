30929cd9557b8f88330f2db8c39854fc841ab0c6
swar1999 <muzan22kris@gmail.com>
2023-09-26 15:21:05 -0700

    task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,10 @@
+#include "lists"
+/**
+ * print_listint_safe - print the linked list
+ * @head : struct paramter
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int counter = 0;
+
