d59a20430ab1bfa2e702b74ccad6b298eacff3c3
swar1999 <muzan22kris@gmail.com>
2023-09-27 00:56:30 -0700

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "main.h"
+int strlength(char *s);
+int strpal(char *s, int i, int len);
+/**
+ * is_palindrome - function that check for string if palindrom
+ * @s: string to be checked
+ * Return: 1 if sucess and 0 if failed
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)/*there is no string*/
+		return (1);/*failed to check*/
+	return (strpal(s, 0, strlength(s)));
+}
+/**
+ * strlength - function that checks the string length
+ * @s: string to be checked
+ * Return: 1 if success and 0 if failed
+ */
+int strlength(char *s)
+{
+	if (*s == '\0')/*reached the string end*/
+		return (0);/*stop recursion*/
+	return (1 + strlength(s + 1));/*move to next string*/
+}
+/**
+ * strpal - function check pailndrom of strings
+ * @s: string to be checked
+ * @i: counter
+ * @len: length of string
+ * Return: 1 if success and 0 if failed
+ */
+int strpal(char *s, int i, int len)
+{
+	if (s[i] != s[len - i - 1])/*check each letter in string*/
+		return (0);
+	if (i >= len)/*string to check*/
+		return (1);
+	return (strpal(s, i + 1, len - 1));
+}
