ef1dee219e8358b1ffacfecf1e0c7dc673970bcd
swar1999 <muzan22kris@gmail.com>
2023-09-28 15:24:06 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _sqrt(int n, int i);
 /**
  * _sqrt_recursion - function for square root
  * @n: number
@@ -6,18 +7,18 @@
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt_recursions(n, 1));
+	return (_sqrt(n, 1));
 }
 /**
- * _sqrt_recursions - function giver the square root of num
+ * _sqrt - function giver the square root of num
  * @n : number to calculate the root
  * @i : ittreation
  * Return: the root of n
  */
-int _sqrt_recursions(int n, int i)
+int _sqrt(int n, int i)
 {
 	if (i * i > n)
 		return (-1); /*not root*/
 	if (i * i == n)
-		return (_sqrt_recursion(n, i + 1));
+		return (_sqrt(n, i + 1));
 }
