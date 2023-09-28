463b376716939d6f33248aa7419f41cf5724a65d
swar1999 <muzan22kris@gmail.com>
2023-09-28 14:09:48 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')/*end of string*/
 	{
-		_print_rev_recursion(*s + 1);/*move to the next char*/
+		_print_rev_recursion(s + 1);/*move to the next char*/
 		_putchar(*s);
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
