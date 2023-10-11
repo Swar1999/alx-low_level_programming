55dbb245483648418d1775f400bdc9771184f82d
swar1999 <muzan22kris@gmail.com>
2023-10-11 12:44:42 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: program that print file name
+ * Return: always 0 (success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
