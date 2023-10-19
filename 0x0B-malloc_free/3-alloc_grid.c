b54f8ccdc8d42d422566a382ac669579579796e1
swar1999 <muzan22kris@gmail.com>
2023-10-19 15:18:51 -0700

    task 3 ,4

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main .h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - function that returns a pointer to a 2
+ * dimensional array of integers
+ * @width: with of array
+ * @height: heigth of array
+ * Return: the address of array
+ */
+int **alloc_grid(int width, int height)
+{
+	int x, y;
+	int **ary;
+		if (height <= 0 || width <= 0)/*frist check*/
+			return (NULL);
+		ary = malloc(sizeof(height * sizeof(int *)));/*allocat memory*/
+		if (ary == NULL)/*malloc failed*/
+		{
+			free(ary);
+			return (NULL);
+		}
+		for (x = 0; x < height; x++)
+		{
+			ary[x] = malloc(width * sizeof(int));
+			if (ary[x] == NULL)
+			{
+				for (x--; x >= 0; x--)
+				{
+					free(ary);
+					free(ary[x]);
+					return (NULL);
+				}
+			}
+		}
+		for (x = 0; x < height; x++)
+		{
+			for (y = 0; y < height; y++)
+				ary[x][y] = 0;
+		}
+		return (ary);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - function to free the new allocation
+ * @grid: pointer;
+ * @height: paramter
+ * Return:nothing
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != NULL)
+	{
+		while (height >= 0)
+		{
+			free(height);
+			height--;
+		}
+		free(grid);
+	}
+}
