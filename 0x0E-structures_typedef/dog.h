4daadb9bb39433f70120873e5c9a1d7374577335
swar1999 <muzan22kris@gmail.com>
2023-10-21 11:38:35 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct with 3 elements
+ * @name : frist element
+ * @age : second element
+ * @owner : thired element
+ * Description: just definition
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
