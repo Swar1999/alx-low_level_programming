91b1e8accf984da7b18122f9930740f706fac20c
swar1999 <muzan22kris@gmail.com>
2023-10-24 12:33:51 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len - function that gives the length of list
+ * @h : struct paramter
+ * Return: number of elements;
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+		while (h)
+		{
+			counter++;
+			h = h->next;/*move to next node*/
+		}
+		return (counter);
+}i
