6a9074d961fdf8a822c681093368a3a1c37c504d
swar1999 <muzan22kris@gmail.com>
2023-10-30 15:41:45 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t count = 0, length;
 		if (filename == NULL)
 			return (-1);
-		file_des = open(filename, O_WRONLY | O_APPAND);
+		file_des = open(filename, O_WRONLY | O_APPEND);
 		if (file_des == -1)
 			return (-1);
 		for (i = 0; text_content[i] != '\0'; i++)
