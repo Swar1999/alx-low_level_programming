2a6b4256fbe61403c59db5aceb14b8bc8139b0f3
swar1999 <muzan22kris@gmail.com>
2023-10-30 15:27:45 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t count = 0, length = 0;
 		if (filename == NULL)
 			return (-1);
-		file_des = open(filename, O_WONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (file_des == -1)
 			return (-1);
 		for (i = 0; text_content[i] != '\0'; i++)
